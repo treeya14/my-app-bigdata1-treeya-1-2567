557e993e5b37847d6b05802e7ad8cd56fda38b8b
Treeya14 <triyasaenjan@gmail.com>
2024-07-19 12:38:06 +0700

    first commit

---
+++ src/app/evaluation/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Evaluation from '@/components/Evaluation'
+
+export default function page() {
+  return (
+    <div>
+        <Evaluation/>
+    </div>
+  )
+}
+
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,9 +25,9 @@ export default function Navbar() {
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="/ai">AI ตรวจวินิจฉัยโรค</a>
               </li>
-              {/* <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="#">การประเมินนโยบายสาธารณะ</a>
-              </li> */}
+              <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
+                <a href="/evaluation">การประเมินนโยบายสาธารณะ</a>
+              </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="#">ChatGPT</a>
               </li>
